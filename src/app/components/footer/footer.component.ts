4f146dbf519840b0eb019c76257d7f1f0c1fb3e3
nimaPhuntsho <nimafunso@gmail.com>
2023-03-21 23:20:33 +1000

    home page design

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.css'],
 })
 export class FooterComponent {
+  year: number = 0;
 
+  ngOnInit() {
+    this.getYear();
+  }
+
+  getYear() {
+    let currentYear = new Date();
+    this.year = currentYear.getFullYear();
+  }
 }
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -12,35 +12,34 @@ export class MenuComponent {
       productID: 1,
       price: 5.5,
       description:
-        'A mocha coffee comfortably takes its place in the crossroads between a coffee and a hot chocolate. If it is made using dark chocolate it is considered a dark mocha, but there is also a white mocha, made with white chocolate. ',
+        'A mocha coffee comfortably takes its place in the crossroads between a coffee and  ',
     },
     {
       name: 'Mocha',
       productID: 1,
       price: 5.5,
       description:
-        'A mocha coffee comfortably takes its place in the crossroads between a coffee and a hot chocolate. If it is made using dark chocolate it is considered a dark mocha, but there is also a white mocha, made with white chocolate. ',
+        'A mocha coffee comfortably takes its place in the crossr white chocolate. ',
     },
     {
       name: 'Mocha',
       productID: 1,
       price: 5.5,
       description:
-        'A mocha coffee comfortably takes its place in the crossroads between a coffee and a hot chocolate. If it is made using dark chocolate it is considered a dark mocha, but there is also a white mocha, made with white chocolate. ',
+        'A mocha coffee comfortably takes its place in the crossrocolate. ',
+    },
+    {
+      name: 'Mocha',
+      productID: 1,
+      price: 5.5,
+      description: 'A mocha coffee comfortably takes its place in the crate. ',
     },
     {
       name: 'Mocha',
       productID: 1,
       price: 5.5,
       description:
-        'A mocha coffee comfortably takes its place in the crossroads between a coffee and a hot chocolate. If it is made using dark chocolate it is considered a dark mocha, but there is also a white mocha, made with white chocolate. ',
-    },
-    {
-      name: 'Mocha',
-      productID: 1,
-      price: 5.5,
-      description:
-        'A mocha coffee comfortably takes its place in the crossroads between a coffee and a hot chocolate. If it is made using dark chocolate it is considered a dark mocha, but there is also a white mocha, made with white chocolate. ',
+        'A mocha coffee comfortably takes its place in the croscolate. ',
     },
   ];
 }
