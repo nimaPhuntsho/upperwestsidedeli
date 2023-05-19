b7f0e19cb920cc5ff2a06f106d50bac8b901b911
nimaPhuntsho <nimafunso@gmail.com>
2023-05-19 16:47:39 +1000

    payment gateway minor changes

--- src/app/components/success/success.component.ts
+++ src/app/components/success/success.component.ts
@@ -1,3 +1,4 @@
+import { faL } from '@fortawesome/free-solid-svg-icons';
 import { Component } from '@angular/core';
 import { Order, PaymentService } from './../../payment.service';
 
@@ -8,25 +9,29 @@ import { Order, PaymentService } from './../../payment.service';
 })
 export class SuccessComponent {
   id = '';
+  today = '';
   currentOrder: Order | undefined;
   placeholder = false;
 
   constructor(private dataSale: PaymentService) {}
   ngOnInit() {
+    this.placeholder = true;
+    let now = new Date();
+    this.today = now.toDateString();
+
     let storedId = localStorage.getItem('orderId');
     if (storedId) {
       this.id = storedId;
       console.log(this.id);
     }
+    this.placeholder = false;
 
-    this.placeholder = true;
     this.dataSale.getAllProducts().then((res) => {
       res?.subscribe(async (data) => {
         const result = await data;
         result.forEach((element) => {
           if (element.id === this.id && element.paymentStatus === 'paid') {
             this.currentOrder = element;
-            this.placeholder = false;
           }
         });
       });
